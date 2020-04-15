979c41afcaf429976466ceade860c6314cbd876c
kikemolina3 <57945283+kikemolina3@users.noreply.github.com>
2020-04-15 21:03:49 +0200

    Update 15/04/2020
    15-04-2020. Matrix Multiplication v1.0

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DS_A1_MatrixMultiplication
  Programa en Python para la multiplicación paralela y distribuida de dos matrices
+## Configuración del entorno de ejecución
+ Este programa construído en Python necesita de la versión 3.7 del mismo. Además, previamente a la ejecución del código, hay que tener el paquete pywren-ibm-cloud instalado (link *[aquí][1]*)
+ Además, hace falta incluir la configuración necesaria de COS y CF, en el fichero ~/.pywren_config (situado en el directorio HOME del usuario)
+## Explicación de ejecución del algoritmo
+ El programa invocando en línea de comandos se encarga de generar dos matrices de valores enteros aleatorios entre -5 y 5 y de devolver su producto:
+ 
+ > ./main.py m n l w
+ 
+ ,donde:
+ 
+    1. m = numero de filas de la 1a matriz
+    2. n = numero de columnas de la 1a matriz = numero de filas de la 2a matriz
+    3. l = numero de columnas de la 2a matriz
+    4, w = numero de subdivisiones de cada matriz(1a row-wise, 2a column-wise). El número de workers vendrá determinado por w^2. 
+
+ [1]: https://github.com/pywren/pywren-ibm-cloud
---
+++ main.py
@@ -0,0 +1,85 @@
+#! /usr/bin/python3
+import pickle, math, random, time
+import pywren_ibm_cloud as pywren
+import numpy as np
+from sys import argv
+from cos_backend import COSBackend
+
+dic = { 
+        'endpoint': 'https://s3.eu-gb.cloud-object-storage.appdomain.cloud',
+        'access_key': 'a967f6d7cde54815b7c240b14e1f2ee5',
+        'secret_key': 'e2900e1d4b0c0e18a2f8b859ce1750ded442811fe9a313da'
+    }
+
+def push_matrix(m, n, l, w):
+    obj = COSBackend(dic)
+    # Generate random matrix
+    matrix1 = np.random.randint(-5, 6, size=(m, n))
+    matrix2 = np.random.randint(-5, 11, size=(n, l))
+    # Push A submatrix
+    for i in range(0, w):
+        infM = int((i * m) / w)
+        supM = int((((i + 1) * m) / w) - 1)
+        submatrix1 = matrix1[infM:supM+1, :]
+        obj.put_object('prac1', 'A' + str(i) + '.mtx', pickle.dumps(submatrix1))
+    # Push B submatrix
+    for j in range (0, w):
+        infL = int((j * l) / w)
+        supL = int((((j + 1) * l) / w) - 1)
+        submatrix2 = matrix2[:, infL:supL+1]
+        obj.put_object('prac1', 'B' + str(j) + '.mtx', pickle.dumps(submatrix2))
+
+def map_function(i, j):
+    obj2 = COSBackend(dic)
+    # Get submatrix
+    m1 = pickle.loads(obj2.get_object('prac1', 'A' + str(i) + '.mtx'))
+    m2 = pickle.loads(obj2.get_object('prac1', 'B' + str(j) + '.mtx'))
+    # Calculate multiplication
+    result = m1.dot(m2)
+    return result
+
+def reduce_function(results):
+    # Concatenation of submatrix
+    f = []
+    num_results = len(results)
+    r = int(math.sqrt(num_results))
+    g = results[0]
+    for i in range(0, r-1):
+        g = np.concatenate((g, results[i*1+1]), axis=1)
+    f = g
+    for i in range(1, r):
+        g = results[i*r]
+        for j in range(0, r-1):
+            g = np.concatenate((g, results[i*r+j+1]), axis=1)
+        f = np.concatenate((f, g), axis=0)
+    return f
+    #return 0 
+
+if __name__ == "__main__":
+    m = int(argv[1])
+    n = int(argv[2])
+    l = int(argv[3])
+    w = int(argv[4])
+    if (w > m or w > n or w > l or w > 10):
+        print("Worker's number is too big")
+        exit()
+    # Create the executor
+    pw = pywren.ibm_cf_executor()
+    # Matrix generation & push to COS (cloud execution)
+    r = pw.call_async(push_matrix, [m, n, l, w])
+    pw.wait(r)
+    # Create iterdata
+    iterdata = []
+    for i in range(0, w):
+        for j in range (0, w):
+            iterdata.append([i, j])
+    # Calculate in the cloud
+    start_time = time.time()
+    if (w == 1):
+        futures = pw.call_async(map_function, [0, 0])
+    else:
+        futures = pw.map_reduce(map_function, iterdata, reduce_function)
+    pw.wait(futures)
+    elapsed_time = time.time() - start_time
+    print(str(elapsed_time) + " seconds\n")
+    print("RESULTANT MATRIX\n", pw.get_result())
